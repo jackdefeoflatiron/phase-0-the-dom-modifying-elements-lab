ff4daa4ecec9237a119bf4a3382f42f203ee9879
John DeFeo <jackd3243@gmail.com>
2021-10-25 13:15:30 -0400

    Completed assignbment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+
+ul.remove('main')
+const ul =(document.querySelector('main'))
+
+h1.textContent = id = "victory"
+console.log(h1.textContent)
+const victory = document.getElementById("victory")
+victory.innerHTML = "Jack is the champion"
+ 
